d9965a18beef4197606f916689f4d9f5e6428034
GitHub Action <action@github.com>
2021-09-29 02:26:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/xukeek/intellijsample/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xukeek/intellijsample/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xukeek.intellijsample.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xukeek.intellijsample.MyBundle
 
 class MyApplicationService {
 
